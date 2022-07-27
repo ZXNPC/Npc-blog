d1cd079c7549413197f86c27e82733fd56cb7f7f
ZXNPC <993023569@qq.com>
2022-07-27 11:00:52 +0800

    git init

---
+++ src/main/java/com/example/blognpc/BlogNpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.blognpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogNpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogNpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blognpc/BlogNpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blognpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogNpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
